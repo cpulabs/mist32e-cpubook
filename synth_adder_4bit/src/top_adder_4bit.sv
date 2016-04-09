1a597ac9ec903d3352516e1745674786c4ce9c5c
cpulabs <takahiro@open-arch.org>
2016-04-10 02:37:36 +0900

    synth_adder_4bitのtop_adder_4bit.svの記述が本の内容と違い、DE0-CVでは正常に動作しなく、DE2-115でもほかのLEDが光ってしまう問題を修正しました。

--- synth_adder_4bit/src/top_adder_4bit.sv
+++ synth_adder_4bit/src/top_adder_4bit.sv
@@ -3,7 +3,7 @@ module top_adder_4bit(
 		input wire CLOCK_50,
 		input wire [17:0] SW,
 		input wire [3:0] KEY,
-		output wire [7:0] LEDG
+		output wire [9:0] LEDR
 	);
 
 
@@ -26,12 +26,12 @@ module top_adder_4bit(
 	adder_4bit ADDER(
 		.iCLOCK(CLOCK_50),
 		.inRESET_SYNC(b_key_buff1[0]),
-		.iDATA_A(b_sw_buff1[17:14]),
+		.iDATA_A(b_sw_buff1[7:4]),
 		.iDATA_B(b_sw_buff1[3:0]),
 		.oDATA(out_data[3:0]),
 		.oC(out_data[4])
 	);
 
-	assign LEDG = {3'h0, out_data};
+	assign LEDR = {5'h0, out_data};
 
 endmodule
